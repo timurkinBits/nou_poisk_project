00a5c2994b39937a29db282a8e8e4be835544fbe
timonkin <timonkin08@gmail.com>
2025-08-25 12:25:02 +0600

    подробная информация по центру

--- src/components/card_detail/CardDetail.jsx
+++ src/components/card_detail/CardDetail.jsx
@@ -5,15 +5,11 @@ export const CardDetail = ({name, surname, fathername, birthdate, imageUrl, work
     <div className='card-detail'>
         <img src={imageUrl} alt="фото" width='250px' height='250px' className='card-image' draggable="false"/>
         <div className='card-data'>
-            <div>
-                <h1 className='card-header'>{surname}</h1>
-                <h1 className='card-header'>{name} {fathername}</h1>
-                <p>{birthdate}</p>
-            </div>
-            <div className='card-work-data'>
-                <p>{workingPlace}</p>
-                <p>{grade}</p>
-            </div>
+            <h1 className='card-header'>{surname}</h1>
+            <h1 className='card-header'>{name} {fathername}</h1>
+            <p>{birthdate}</p>
+            <p>{workingPlace}</p>
+            <p>{grade}</p>
         </div>
     </div>
     )
